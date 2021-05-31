00246da51a47bfb8a4abd19ca9d29b4b4f5abeec
esteloabell <manitobaaa@gmail.com>
2021-05-31 16:50:09 -0500

    Create cars.js

---
+++ assets/data/cars.js
@@ -0,0 +1,33 @@
+import bentayga1 from '../images/bently/bentayga3.jpg';
+import bentayga2 from '../images/bently/bentayga3.jpg';
+import bentayga3 from '../images/bently/bentayga3.jpg';
+import bentayga4 from '../images/bently/bentayga3.jpg';
+import bentayga5 from '../images/bently/bentayga3.jpg';
+import bentayga6 from '../images/bently/bentayga3.jpg';
+
+
+const cars = [
+  {
+    id: 1,
+    title: 'Car1',
+    user: 'Bently',
+    background: bentayga3,
+    avatars: [
+      {
+        id: 1,
+        image: bentayga1
+      },
+      {
+        id: 2,
+        image: bentayga2
+      },
+      {
+        id: 3,
+        image: bentayga3
+      },
+    ]
+  },
+
+];
+
+export default cars;
