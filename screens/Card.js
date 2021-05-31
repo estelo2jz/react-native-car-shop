5c7b16e3a69988c4d5783e4f76d78a62fb3be16e
esteloabell <manitobaaa@gmail.com>
2021-05-31 16:50:19 -0500

    Create Card.js

---
+++ screens/Card.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { View, Text, ImageBackground, StyleSheet, Pressable } from 'react-native';
+import theme from '../assets/themes';
+
+const Card = ({ item, navigation }) => {
+  return (
+    <ImageBackground
+      source={item.background}
+      style={styles.imageBackground}
+    >
+      <Pressable
+        style={styles.imageContentContainer}
+        onPress={() => navigation.navigate("Shared Album", {
+          album: item
+        })}
+      >
+        <View>
+          <View>
+            <Text style={styles.imageTitle}>{item.title}</Text>
+            <Text style={styles.imageSubtitle}>{item.user}</Text>
+          </View>
+        </View>
+      </Pressable>
+    </ImageBackground>
+  )
+}
+
+const styles = StyleSheet.create({
+  imageBackground: {
+    // resizeMode: 'cover',
+    // overflow: 'hidden',
+    // height: theme.imageHeight.s,
+    // marginTop: theme.spacing.m,
+    // marginHorizontal: theme.spacing.m,
+    // paddingHorizontal: theme.spacing.m,
+    // borderRadius: theme.borderRadius.m,
+    // justifyContent: 'center',
+  },
+  imageContentContainer: {
+    // flexDirection: 'row',
+    // alignItems: 'center',
+    // justifyContent: 'space-between',
+  },
+  imageTitle: {
+    // ...theme.textVariants.h1,
+    // color: theme.colors.white,
+  },
+  imageSubtitle: {
+    // ...theme.textVariants.body2,
+    // color: theme.colors.white,
+  }
+})
+
+export default Card;
