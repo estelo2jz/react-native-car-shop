3240c556924ff3352b6fd38e523101f49a691620
esteloabell <manitobaaa@gmail.com>
2021-05-31 16:50:27 -0500

    Update ProductScreen.js

--- screens/ProductScreen.js
+++ screens/ProductScreen.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, ScrollView } from 'react-native';
+import theme from '../assets/themes/index';
+import cars from '../assets/data/cars';
+import Card from './Card';
+import Separator from './Separator.js';
 
-const ProductScreen = () => {
+const ProductScreen = ({ navigation, navigation: { setOptions } }) => {
   return (
     <View style={styles.container}>
       <Text>Product Screen</Text>
+
+      <ScrollView>
+        <View style={styles.albumContainer}>
+          {cars.map((item, index) =>
+            <View key={index}>
+              <Card item={item} navigation={navigation} />
+
+              {/* add separator if the index is 1 */}
+              {index === 1 && <Separator />}
+            </View>
+
+          )}
+        </View>
+      </ScrollView>
+
       {/* <Button
         title="Click Here"
         onPress={() => alert('Button Clicked!')}
