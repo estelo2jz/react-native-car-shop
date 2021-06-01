b1a5606a3a9c5a097a3ac96c67d9087edb5db401
esteloabell <manitobaaa@gmail.com>
2021-05-31 20:37:57 -0500

    added data for cars

--- assets/data/cars.js
+++ assets/data/cars.js
@@ -1,15 +1,62 @@
-import bentayga1 from '../images/bently/bentayga3.jpg';
-import bentayga2 from '../images/bently/bentayga3.jpg';
+import bentayga1 from '../images/bently/bentayga1.jpg';
+import bentayga2 from '../images/bently/bentayga2.jpg';
 import bentayga3 from '../images/bently/bentayga3.jpg';
 import bentayga4 from '../images/bently/bentayga3.jpg';
 import bentayga5 from '../images/bently/bentayga3.jpg';
 import bentayga6 from '../images/bently/bentayga3.jpg';
+import continental1 from '../images/bently/continental1.jpg';
+import continental2 from '../images/bently/continental2.jpg';
+import continental3 from '../images/bently/continental3.jpg';
+import continental4 from '../images/bently/continental4.jpg';
+import continental5 from '../images/bently/continental5.jpg';
+import continental6 from '../images/bently/continental6.jpg';
+import spur1 from '../images/bently/spur1.jpg';
+import spur2 from '../images/bently/spur2.jpg';
+import spur3 from '../images/bently/spur3.jpg';
+import spur4 from '../images/bently/spur4.jpg';
+import spur5 from '../images/bently/spur5.jpg';
+import spur6 from '../images/bently/spur6.jpg';
 
+import phantom1 from '../images/rollroyce/phantom1.jpeg';
+import phantom2 from '../images/rollroyce/phantom2.jpeg';
+import phantom3 from '../images/rollroyce/phantom3.jpeg';
+import phantom4 from '../images/rollroyce/phantom4.jpeg';
+import phantom5 from '../images/rollroyce/phantom5.jpeg';
+import phantom6 from '../images/rollroyce/phantom6.jpeg';
+import cullinan1 from '../images/rollroyce/cullinan1.jpg';
+import cullinan2 from '../images/rollroyce/cullinan2.jpg';
+import cullinan3 from '../images/rollroyce/cullinan3.jpg';
+import cullinan5 from '../images/rollroyce/cullinan5.jpg';
+import cullinan6 from '../images/rollroyce/cullinan6.jpg';
+import ghost1 from '../images/rollroyce/ghost1.png';
+import ghost2 from '../images/rollroyce/ghost2.jpg';
+import ghost3 from '../images/rollroyce/ghost3.jpg';
+import ghost4 from '../images/rollroyce/ghost4.jpg';
+import ghost5 from '../images/rollroyce/ghost5.jpg';
+import ghost6 from '../images/rollroyce/ghost6.jpg';
+import wraith1 from '../images/rollroyce/wraith1.jpg';
+import wraith2 from '../images/rollroyce/wraith2.jpg';
+import wraith3 from '../images/rollroyce/wraith3.jpg';
+import wraith4 from '../images/rollroyce/wraith4.jpg';
+import wraith5 from '../images/rollroyce/wraith5.jpg';
+import wraith6 from '../images/rollroyce/wraith6.jpg';
+import dawn1 from '../images/rollroyce/dawn1.jpg';
+import dawn2 from '../images/rollroyce/dawn2.jpg';
+import dawn3 from '../images/rollroyce/dawn3.jpg';
+import dawn4 from '../images/rollroyce/dawn4.jpg';
+import dawn5 from '../images/rollroyce/dawn5.jpg';
+import dawn6 from '../images/rollroyce/dawn6.jpg';
+import blackbadge1 from '../images/rollroyce/blackbadge1.jpg';
+import blackbadge2 from '../images/rollroyce/blackbadge2.jpg';
+import blackbadge3 from '../images/rollroyce/blackbadge3.jpg';
+import blackbadge4 from '../images/rollroyce/blackbadge4.jpg';
+import blackbadge5 from '../images/rollroyce/blackbadge5.jpg';
+import blackbadge6 from '../images/rollroyce/blackbadge6.jpg';
 
 const cars = [
   {
     id: 1,
-    title: 'Car1',
+    title: 'Bentayga',
     user: 'Bently',
     background: bentayga3,
     avatars: [
@@ -23,7 +70,240 @@ const cars = [
       },
       {
         id: 3,
-        image: bentayga3
+        image: bentayga4
+      },
+      {
+        id: 4,
+        image: bentayga5
+      },
+      {
+        id: 5,
+        image: bentayga6
+      },
+    ]
+  },
+  {
+    id: 2,
+    title: 'Continental',
+    user: 'Bently',
+    background: continental1,
+    avatars: [
+      {
+        id: 1,
+        image: continental2
+      },
+      {
+        id: 2,
+        image: continental3
+      },
+      {
+        id: 3,
+        image: continental4
+      },
+      {
+        id: 4,
+        image: continental5
+      },
+      {
+        id: 5,
+        image: continental6
+      },
+    ]
+  },
+  {
+    id: 3,
+    title: 'Flying SPUR',
+    user: 'Bently',
+    background: spur1,
+    avatars: [
+      {
+        id: 1,
+        image: spur6
+      },
+      {
+        id: 2,
+        image: spur3
+      },
+      {
+        id: 3,
+        image: spur4
+      },
+      {
+        id: 4,
+        image: spur5
+      },
+      {
+        id: 5,
+        image: spur2
+      },
+    ]
+  },
+
+
+
+
+
+  {
+    id: 4,
+    title: 'Phantom',
+    user: 'Rolls Royce',
+    background: phantom1,
+    avatars: [
+      {
+        id: 1,
+        image: phantom2
+      },
+      {
+        id: 2,
+        image: phantom3
+      },
+      {
+        id: 3,
+        image: phantom4
+      },
+      {
+        id: 4,
+        image: phantom5
+      },
+      {
+        id: 5,
+        image: phantom6
+      },
+    ]
+  },
+  {
+    id: 5,
+    title: 'Cullinan',
+    user: 'Rolls Royce',
+    background: cullinan2,
+    avatars: [
+      {
+        id: 1,
+        image: cullinan1
+      },
+      {
+        id: 2,
+        image: cullinan3
+      },
+      {
+        id: 3,
+        image: cullinan5
+      },
+      {
+        id: 4,
+        image: cullinan6
+      },
+    ]
+  },
+  {
+    id: 6,
+    title: 'Ghost',
+    user: 'Rolls Royce',
+    background: ghost6,
+    avatars: [
+      {
+        id: 1,
+        image: ghost2
+      },
+      {
+        id: 2,
+        image: ghost3
+      },
+      {
+        id: 3,
+        image: ghost4
+      },
+      {
+        id: 4,
+        image: ghost5
+      },
+      {
+        id: 5,
+        image: ghost1
+      },
+    ]
+  },
+  {
+    id: 7,
+    title: 'Wraith',
+    user: 'Rolls Royce',
+    background: wraith1,
+    avatars: [
+      {
+        id: 1,
+        image: wraith2
+      },
+      {
+        id: 2,
+        image: wraith3
+      },
+      {
+        id: 3,
+        image: wraith4
+      },
+      {
+        id: 4,
+        image: wraith5
+      },
+      {
+        id: 5,
+        image: wraith6
+      },
+    ]
+  },
+  {
+    id: 8,
+    title: 'Dawn',
+    user: 'Rolls Royce',
+    background: dawn5,
+    avatars: [
+      {
+        id: 1,
+        image: dawn2
+      },
+      {
+        id: 2,
+        image: dawn3
+      },
+      {
+        id: 3,
+        image: dawn4
+      },
+      {
+        id: 4,
+        image: dawn1
+      },
+      {
+        id: 5,
+        image: dawn6
+      },
+    ]
+  },
+  {
+    id: 9,
+    title: 'Black Badge',
+    user: 'Rolls Royce',
+    background:blackbadge2,
+    avatars: [
+      {
+        id: 1,
+        image:blackbadge1
+      },
+      {
+        id: 2,
+        image:blackbadge3
+      },
+      {
+        id: 3,
+        image:blackbadge4
+      },
+      {
+        id: 4,
+        image:blackbadge5
+      },
+      {
+        id: 5,
+        image:blackbadge6
       },
     ]
   },
